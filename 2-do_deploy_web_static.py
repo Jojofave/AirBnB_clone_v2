90b4e250df94dcfba0eec2813de679ae9c2a1db8
jojofave <ozonahebere@gmail.com>
2023-07-06 02:53:09 -0700

    airbnb

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,53 @@
-#!/usr/bin/python3
-""" Function that compress a folder """
-from datetime import datetime
-from fabric.api import *
-import shlex
+#!/usr/bin/env python3
+"""
+Fabric script that distributes an archive to your web servers
+"""
 import os
+from fabric.api import env, put, run
+from fabric.decorators import task
+
+env.hosts = ['54.209.118.161', '54.165.214.4']
 
 
-env.hosts = ['35.231.33.237', '34.74.155.163']
-env.user = "ubuntu"
-
-
+@task
 def do_deploy(archive_path):
-    """ Deploys """
+    """
+    Distributes an archive to your web servers
+    """
     if not os.path.exists(archive_path):
         return False
+
     try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
-
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
+        # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
+
+        # Uncompress the archive to the folder
+        # /data/web_static/releases/<archive filename without extension> on the web server
+        archive_filename = os.path.basename(archive_path)
+        archive_folder = "/data/web_static/releases/" + archive_filename.split(".")[0]
+        run("mkdir -p {}".format(archive_folder))
+        run("tar -xzf /tmp/{} -C {}".format(archive_filename, archive_folder))
+
+        # Delete the archive from the web server
+        run("rm /tmp/{}".format(archive_filename))
+
+        # Move files from web_static folder to the current release folder
+        run("mv {}/web_static/* {}/".format(archive_folder, archive_folder))
+
+        # Remove the web_static folder
+        run("rm -rf {}/web_static".format(archive_folder))
+
+        # Delete the symbolic link /data/web_static/current from the web server
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
+
+        # Create a new symbolic link /data/web_static/current on the web server
+        # linked to the new version of your code (/data/web_static/releases/<archive filename without extension>)
+        run("ln -s {} /data/web_static/current".format(archive_folder))
+
         print("New version deployed!")
         return True
-    except:
+
+    except Exception as e:
+        print(e)
         return False
+
