ec9f130fd56c5d1c4583e09f8d40117615768ccb
jojofave <ozonahebere@gmail.com>
2023-07-22 10:46:22 +0000

    web

---
+++ web_flask/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def c_text(text):
+    text = text.replace('_', ' ')
+    return 'C ' + text
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
